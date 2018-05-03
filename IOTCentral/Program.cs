fe62efa915dcd49cbf154d27b578fe7f58b06084
Soynerdito <soynerdito@gmail.com>
2018-05-02 23:28:43 -0400

    still trying

--- IOTCentral/Program.cs
+++ IOTCentral/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace IOTCentral
 {
@@ -8,11 +9,19 @@ namespace IOTCentral
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
+
+         
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            .UseKestrel()
+            .UseConfiguration(new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build())
+            .UseStartup<Startup>()
+            .Build();
     }
 }
