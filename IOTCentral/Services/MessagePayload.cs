5ddeb337646762881b59dcacfe32911693af4c49
Soynerdito <soynerdito@gmail.com>
2018-06-30 22:51:49 -0400

    just adding some comments to the code. we all should comment our code.

--- IOTCentral/Services/MessagePayload.cs
+++ IOTCentral/Services/MessagePayload.cs
@@ -9,9 +9,24 @@ namespace IotCentral.Services
     [JsonObject("message")]
     public class MessageDto
     {
+        /// <summary>
+        /// Information on what to do with this message
+        /// JWT Token with type of device and indication 
+        /// of workflow to handle it
+        /// </summary>
+        [JsonProperty("action_token")]
+        public string ActionToken { get; set; }
+        /// <summary>
+        /// Free string for payload to be deliverd.
+        /// Receiving end should be able to handle this payload.
+        /// Message handlers should be able to handle this message
+        /// </summary>
         [JsonProperty("payload")]
         public string RawPayload { get; set; }
-        [JsonProperty("expiration_gmt0")]
+        /// <summary>
+        /// If message has not been deleted by then delete it. Forger about it
+        /// </summary>
+        [JsonProperty("expiration_gmt_0")]
         public DateTime? ExpirationGmt0 { get; set; }
     }
 
@@ -20,9 +35,24 @@ namespace IotCentral.Services
     /// </summary>
     public class MessagePayload
     {
+        /// <summary>
+        /// Inforation should come from access token
+        /// </summary>
         public int OwnnerId { get; set; }
+        /// <summary>
+        /// The id of the account that sends the message
+        /// </summary>
         public Guid OwnerAccount { get; set; }
+        /// <summary>
+        /// When the message arrived to this application.
+        /// Time not necessary the same as when the message 
+        /// was created in the client side.
+        /// </summary>
         public DateTime RecevTimestamp { get; set; }
+        /// <summary>
+        /// Nothing changed, message as it was received.
+        /// Should be kept pure to ensure destin has correct data.
+        /// </summary>
         public MessageDto RawMessage { get; set; }
 
     }
